5068a778fbfd32b840c654a529a457b50f3c1afa
Joseph <jjoseairtonribeiro@gmail.com>
2022-06-27 16:53:18 -0300

    Update 27/06

--- Class Schedule.py
+++ Class Schedule.py
@@ -1,16 +1,24 @@
-from Class Contact import *
+from Contact import *
 
 class Schedule:
     def __init__(self):
         self.contact_list = []
 
     def save_contact(self):
-        ent_name = input('Insira o nome: ')
         ent_cod = input('Insira o código: ')
+        ent_name = input('Insira o nome: ')
         ent_phone = input('Insira o telefone: ')
         self.contact_list.append(Contact(ent_cod, ent_name, ent_phone))
         print('DATA SAVED!')
 
     def list_cont_all(self):
         for i in range(len(self.contact_list))
-        print('Nome: 'self.contact_list[i].name, 'Telefone: 'self.contact_list[i].phone,'Código: 'self.contact_list[i].cod)
+        print('Nome: ',self.contact_list[i].name, 'Telefone: ',self.contact_list[i].phone,'Código: ',self.contact_list[i].cod)
+
+    def change_phone(self):
+        entrada2 = input('Informe o código do contato')
+        for i in range(len(self.contact_list)):
+            if entrada1 == self.contact_list[i].cod:
+                self.contact_list[i].phone = input('Novo Numero: ')
+            else:
+                print('Contato não encontrado')
--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
-from Class Contact import *
-from Class Schedule import *
+from Contact import *
+from Schedule import *
 
 schedule_obj = Schedule()
 schedule_obj.save_contact()
@@ -9,7 +9,55 @@ schedule_obj.list_cont_all()
 
 
 
-...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 white = '#ffffff'
 dark_bl = '#00357b'
 bl = '#0F0F10'
@@ -22,7 +70,10 @@ Menu_1.title('Schedule')
 
 frame_fav = Frame(Menu_1)  #Frame 1 AKA f1
 frame_main = Frame(Menu_1) #Frame 2 AKA f2
-
+frame_main.pack()
+frame_fav.pack()
 
 lb_f1 = Frame(Menu)
-...
+lb_f1.pack()
+mainloop()
+'''
