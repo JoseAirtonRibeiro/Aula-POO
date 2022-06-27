134e6d0709e6647ad5250d66069bb507b7c1822c
Joseph <jjoseairtonribeiro@gmail.com>
2022-06-27 16:53:42 -0300

    Update 27/06

--- Class Menu.py
+++ Class Menu.py
@@ -1,5 +1,5 @@
-from Class Contact import *
-from Class Schedule import *
+from Contact import *
+from Schedule import *
 from tkinter import *
 import mysql.connector
 #_______________________
@@ -8,17 +8,18 @@ def __init__(self):
     schedule_obj = Schedule()
 
     while True:
-        start = input('Informe opção desejada\n
-                       '[1] - Novo contato\n'
-                       '[2] - Listar Contatos\n'
-                       '[0] - Sair\n')
+        entrada1 = input('Informe opção desejada\n
+                         '[1] - Novo contato\n'
+                         '[2] - Listar Contatos\n'
+                         '[3] - Alterar Telefone\n'
+                         '[0] - Sair\n')
         if start == '1':
             schedule_obj.save_contact()
         elif start == '2':
             schedule_obj.list_cont_all()
+        elif start == '3':
+            pass
         elif start == '0':
             break
         else:
             print('Opção Inválida')
-
-
